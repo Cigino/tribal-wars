162e19ad44b9160cdfc6e2397d6e638dfee98300
Cigino <daniellasik@gmail.com>
2026-02-02 23:47:00 +0100

    loader.js

--- loader.js
+++ loader.js
@@ -114,8 +114,7 @@ async function generateScript(){
     `;
     let key = CryptoJS.AES.encrypt(plainText, "automateThisAnnoyingPart").toString();
 
-    let outputScript = `javascript:var encryptedData='${key}';$.getScript('https://raw.githubusercontent.com/El-Cigino/fake-script/main/fakeScriptMain.js');void(0);`;
-
+    let outputScript = `javascript:var encryptedData='${key}';$.getScript('https://cdn.jsdelivr.net/gh/El-Cigino/fake-script@main/fakeScriptMain.js');
     $("#input_link_script").val(outputScript);
     alert("Script generated! Copy the link above to run.");
 }
